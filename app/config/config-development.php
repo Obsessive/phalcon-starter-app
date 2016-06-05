62aa6e71377e854fa2c237f94f8b1bb3486b4d2a
vickofranic <vfranic@gmail.com>
2016-06-05 16:00:45 +0200

    Config updated, facebook added

--- app/config/config-development.php
+++ app/config/config-development.php
@@ -7,18 +7,23 @@ return [
 		'host'        => 'localhost',
 		'username'    => '',
 		'password'    => '',
-		'dbname'      => 'database',
+		'dbname'      => 'database'
 	],
 	'application' => [
-		'controllersDir' => __DIR__ . '/../../app/controllers/',
-		'modelsDir'      => __DIR__ . '/../../app/models/',
-		'viewsDir'       => __DIR__ . '/../../app/views/',
-		'baseUri'        => '/',
+		'controllersDir' 	=> __DIR__ . '/../../app/controllers/',
+		'modelsDir'      	=> __DIR__ . '/../../app/models/',
+		'viewsDir'       	=> __DIR__ . '/../../app/views/',
+		'host'				=> 'http://myDomain.dev'
 	],
     'volt' => [
         'path' => __DIR__ . '/../volt/',
         'extension' => '.compiled',
         'separator' => '@',
         'stat' => true
+    ],
+    'facebook' => [
+    	'app_id'				=> 'APP_ID',
+    	'app_secret'			=> 'APP_SECRET',
+    	'default_graph_version' => 'v2.5',
     ]
 ];
